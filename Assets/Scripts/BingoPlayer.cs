c646ea8e2427453af9b3d007e0e3953c31975641
George Westergaard <rocketguy124@gmail.com>
2022-03-02 01:12:54 -0500

    Added Comments

--- Assets/Scripts/BingoPlayer.cs
+++ Assets/Scripts/BingoPlayer.cs
@@ -24,6 +24,7 @@ public class BingoPlayer : NetworkComponent
 
     public override void HandleMessage(string flag, string value)
     {
+        //Handling the Setting of the Name from the EndEdit of the InputField
         if(flag == "NAME")
         {
             if (IsClient)
@@ -38,6 +39,7 @@ public class BingoPlayer : NetworkComponent
                 SendUpdate("NAME", value);
             }
         }
+        //Handling the Ready-ing of players from the Toggle's callback function
         if(flag == "READY")
         {
             isReady = bool.Parse(value);
@@ -49,16 +51,19 @@ public class BingoPlayer : NetworkComponent
             {
                 SendUpdate("READY", value);
             }
+            //Marking the Local Player as  Blue to further indicate who they are (besides the name they entered, if per chance identical names)
             if (IsLocalPlayer)
             {
                 gameObject.GetComponent<Image>().color = new Color32(0, 0, 255, 128);
             }
         }
         
+        //Handling the Random Numbers for the board to set to the clients once decided on by the server
         if(flag == "RANDNUM")
         {
             if (IsClient)
             {
+                //Takes the value sent in and splits it by commas into an array to then reassign into the clients 2D array
                 string[] strArr = value.Split(',');
                 for (int i = 0; i < 5; i++)
                 {
@@ -69,14 +74,17 @@ public class BingoPlayer : NetworkComponent
                 }
             }
         }
+        //Hanlding the marking the Text object as red if the number on the board was called by the GameMaster
         if(flag == "MARK")
         {
+            //index 0 and 2, since the comma is at 1
             int i = int.Parse(value[0].ToString());
             int j = int.Parse(value[2].ToString());
 
             bingoNumbers[i, j].color = Color.red;
 
         }
+        //Handling changing the winner's color to Yellow-ish Gold for an obvious win
         if(flag == "WIN")
         {
             gameObject.GetComponent<Image>().color = new Color32(224, 108, 22, 128);
@@ -90,34 +98,45 @@ public class BingoPlayer : NetworkComponent
 
     public override IEnumerator SlowUpdate()
     {
+        //Making the other players objects untouchable
         if (!IsLocalPlayer)
         {
             readyToggle.interactable = false;
             nameField.interactable = false;
         }
+        //
         if (IsServer)
         {
             int randNum = 0;
             message = "";
 
-
             Debug.Log("In the SlowUpdate of GameMaster right before object creation");
             for (int i = 0; i < 5; i++)
             {
+                //This will be what handles the prevention of duplicate numbers
+                //The Enumerable will make a List of 1-15, 16-30, 31-45, 46-60, 61-75 (Each one at a time) values and place a random value on all of them by index
+                //The List is then organized by their weight/value
+                //This means that technically, the random numbers are already decided and not random each time, but they will be random every instance ran
+                //Since only 1-15 (or the other ranges) are made, there will be no duplicated numbers
+                //Only the first five numbers of this ordered List are taken and assigned to the board
                 var randomNumbers = Enumerable.Range(i*15+1, 15).OrderBy(x => Random.value).Take(5).ToList();
 
                 for (int j = 0; j < 5; j++)
                 {
                     randNum = randomNumbers[j];
+                    //setting the text to the random number from the List
                     bingoNumbers[i, j].text = randNum.ToString();
+                    //creating a long string (combined with commas to be split) to then send to be handled from Server to send the correct numbers to the clients
                     message += randNum.ToString() + "," ;
                 }
                 
             }
+            //This is how the Server will relay the correct numbers to the Clients
             SendUpdate("RANDNUM",message);
         }
         while (IsConnected)
         {
+            //Checking for a name before allowing the player to ready up
             if (IsLocalPlayer)
             {
                 if (!isNamed)
@@ -148,6 +167,7 @@ public class BingoPlayer : NetworkComponent
     // Start is called before the first frame update
     void Start()
     {
+        //My prefab is only a Panel, so I will find a Canvas premade in the scene to assign the players to (in the else)
         GameObject temp = GameObject.Find("GameCanvas");
 
         if (temp == null)
@@ -159,6 +179,8 @@ public class BingoPlayer : NetworkComponent
             this.transform.SetParent(temp.transform);
         }
         
+        //Standard nested loop to instantiate 25 Text objects on the object that has this script (Essentially the player)
+        //It also assigns them onto a premade panel that will have a  grid layout to better organize the board
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
@@ -175,6 +197,8 @@ public class BingoPlayer : NetworkComponent
     {
         
     }
+
+    //Callback function for the Name that will send it across Server to Client
     public void OnEndEdit(string s)
     {
         if(IsLocalPlayer && MyId.IsInit)
@@ -186,6 +210,7 @@ public class BingoPlayer : NetworkComponent
             }
         }
     }
+    //Callback function for the Toggle
     public void SetReady(bool r)
     {
         if(IsLocalPlayer && MyId.IsInit  && isNamed)
@@ -194,12 +219,14 @@ public class BingoPlayer : NetworkComponent
             Debug.Log(r);
         }
     }
+    //Function to Find a value if it matches the number that  was called by the Gamemaster
     public void FindValue(string s)
     {
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
             {
+                //if the called number matches to what is in the board, it will send the MARK  flag to change the color said match as well as perform a check for a winner
                 if (bingoNumbers[i, j].text == s)
                 {
                     SendUpdate("MARK", i + " " + j);
@@ -210,12 +237,35 @@ public class BingoPlayer : NetworkComponent
             }
         }
     }
+    //Function to Check for a winner once a value has been matched to the number called (This will run every time a value is found)
     public void CheckWinCondition(int i, int j)
     {
+        //All 4 ways to win (No free space and no 4 corners)
+        // XXXXX
         bool horizontal = true;
+        //X
+        //X
+        //X
+        //X
+        //X
         bool vertical = true;
+        //X
+        //  X
+        //    X
+        //      X
+        //        X
         bool Diag1 = true;
+        //        X
+        //      X
+        //    X
+        //  X
+        //X
         bool Diag2 = true;
+
+        //for each row/column do the check
+        //Instead of creating an array that held each coordinate and checking if  the coordinates were in the right  position,
+        //I check if they are just red and consider that a marked number (as it is) and compare its  neighbors through the loop
+        //This comparison is just having the "Innocent until proven guilty" or in this "A Winner until I don't see a red in the right spot"
         for (int y = 0; y < 5; y++)
         {
             if (bingoNumbers[i, y].color != Color.red)
@@ -235,9 +285,12 @@ public class BingoPlayer : NetworkComponent
                 Diag2 = false;
             }
         }
+        //If any of the variables are true, then that means that their check showed that all of the numbers were marked (red) 
+        //Thus, there is a win condition and that player wins.
         if (horizontal || vertical || Diag1 || Diag2)
         {
             SendUpdate("WIN", "you da winnar");
+            //Updates a Static variable to allow interaction between scripts 
             GameMasterScript.winnerFound = true;
         }
     }
--- Assets/Scripts/GameMasterScript.cs
+++ Assets/Scripts/GameMasterScript.cs
@@ -13,17 +13,19 @@ public class GameMasterScript : NetworkComponent
 
     public Text temptext;
 
-    public static bool winnerFound = false;
+    public static bool winnerFound = false; // Static to allow interaction between scripts
     public int randomNum;
 
     public override void HandleMessage(string flag, string value)
     {
         Debug.Log(flag + value);
+        //Handles the Starting of the Game, initiated by the GameMaster once all Clients are connected and ready up (requires >2 players)
         if (flag == "GAMESTART")
         {
             gameStarted = true;
             //Debug.Log("In GameMaster - GAMESTART");
             calledNumberText.gameObject.SetActive(true);
+            //For each object with BingoPlayer Script, set the ready menu to disappear while turning on the Bingo Board and revealing the player's name
             foreach (BingoPlayer bp in GameObject.FindObjectsOfType<BingoPlayer>())
             {
                 bp.transform.GetChild(0).gameObject.SetActive(false);
@@ -32,6 +34,7 @@ public class GameMasterScript : NetworkComponent
                 bp.bingoBoardPanel.SetActive(true);
             }
         }
+        //Handling the displaying of the calling of numbers 
         if (flag == "CALL")
         {
             if (IsServer)
@@ -45,6 +48,7 @@ public class GameMasterScript : NetworkComponent
                 calledNumberText.text = "Number called: " + value;
             }
         }
+        //Handles the Win update, just sets the text to an obvious difference for the win
         if(flag == "WINF")
         {
             calledNumberText.text = "Winner has been found, Server shutting down in 20 Seconds, Thank you for Playing.";
@@ -53,15 +57,15 @@ public class GameMasterScript : NetworkComponent
 
     public override void NetworkedStart()
     {
-        if (IsServer)
-        {
 
-        }
     }
 
     public override IEnumerator SlowUpdate()
     {
+        //Setting the array so that the FindObjectsOfType isn't called repeatedly 
         BingoPlayer[] BingoPlayerarr = FindObjectsOfType<BingoPlayer>();
+
+        //Menu Phase, will not pass till all clients ready
         while (!gameStarted && IsServer)
         {
             bool readyGo = true;
@@ -86,29 +90,38 @@ public class GameMasterScript : NetworkComponent
             yield return new WaitForSeconds(2f);
         }
 
+        //Making sure to update that the Game has started
         if (IsServer)
         {
             Debug.Log("Right before GAMESTART");
             SendUpdate("GAMESTART", gameStarted.ToString());
         }
 
+        //This will be what handles the prevention of duplicate numbers
+        //The Enumerable will make a List of 75 values and place a random value on all of them by index
+        //The List is then organized by their weight/value
+        //This means that technically, the random numbers are already decided and not random each time, but they will be random every instance ran
+        //Since only 75 numbers are made and all aren't duplicates, there will be no duplicated numbers in the number calls (Gameboard is on the other script)
         int index = 0;
         var randomNumbers = Enumerable.Range(1, 75).OrderBy(x => Random.value).ToList();
 
+        //Play Phase -> call numbers, check for winner, if winner then move out to next Phase
         while (IsServer && !winnerFound)
         {
 
             if (gameStarted)
             {
-
+                //Setting a variable to the number from the Enumerable to be called
                 randomNum = randomNumbers[index];
                 index++;
 
                 SendUpdate("CALL", randomNum.ToString());
+                //For each player, run a function in BingoPlayer to check to see if the value called is in their board (This function will call a separate funciton to check for the winner)
                 foreach (BingoPlayer bp in BingoPlayerarr)
                 {
                     bp.FindValue(randomNum.ToString());
                 }
+                //Numbers are called every 1 second
                 yield return new WaitForSeconds(1f);
                 //calledNumberText.text = "Number called: " + randomNum.ToString();
             }
@@ -120,6 +133,7 @@ public class GameMasterScript : NetworkComponent
             }
             yield return new WaitForSeconds(0.1f);
         }
+        //At this point, the Play Phase is over and the winner has been found, so update WINF and wait 20 seconds before disconnecting the Server
         if (IsServer)
         {
             SendUpdate("WINF", "wow");
